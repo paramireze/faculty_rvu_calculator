eae4768386a4f9030a61b50e74c8cdd7c86077bd
Paul <paramireze@gmail.com>
2019-04-10 19:04:59 -0500

    add helper methods to display objects in the log file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # cave man debugging method
+  def dump_data(data, title = "INSPECTING")
+    Rails.logger.debug("<<<<<<-------------#{title}----------->>>>>>>>")
+    Rails.logger.debug("<                                              >")
+    Rails.logger.debug("<                                              >")
+    Rails.logger.debug(" #{data.inspect}                                ")
+    Rails.logger.debug("<                                              >")
+    Rails.logger.debug("<                                              >")
+    Rails.logger.debug("<<<<<<-------------INSPECTING----------->>>>>>>>")
+
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # cave man debugging method
+  def dump_data_helper(data, title = "INSPECTING")
+    Rails.logger.debug("<<<<<<-------------#{title}----------->>>>>>>>")
+    Rails.logger.debug("<                                              >")
+    Rails.logger.debug("<                                              >")
+    Rails.logger.debug(" #{data.inspect}                                ")
+    Rails.logger.debug("<                                              >")
+    Rails.logger.debug("<                                              >")
+    Rails.logger.debug("<<<<<<-------------INSPECTING----------->>>>>>>>")
+  end
+
 end
